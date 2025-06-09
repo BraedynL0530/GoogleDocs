fc1cc94022fab7fd2510f8e03e4ae07d74a531f6
Braedyn L <poohdie10@gmail.com>
2025-06-09 15:52:39 -0400

    User Registration and login created, Decent Ui

--- DocsClone/settings.py
+++ DocsClone/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'GoogleDocs.apps.GoogledocsConfig',
+    'crispy_forms',
+    "crispy_bootstrap4",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -116,9 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
--- GoogleDocs/views.py
+++ GoogleDocs/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+import GoogleDocs
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'Home.html',)
+def register(response):
+    form = UserCreationForm()
+    if response.method == "POST":
+        form = UserCreationForm(response.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")  # or another page
+
+    return render(response, "register.html", {"form": form})
