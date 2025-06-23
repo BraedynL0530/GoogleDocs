e666aae96fff6c0ab7040a8a2160dbbfe0cfb6e6
Braedyn L <poohdie10@gmail.com>
2025-06-23 17:37:15 -0400

    user specific accsss/documents Current features 6/18/25 -Login/user Auth -Realtime collaboration -Periodic saves to DB(Title and Content) -User specific Docs -Doc sharing. -Final put js sepearly -changed secert key, removed db .env stuff for deploying my own website

--- DocsClone/settings.py
+++ DocsClone/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
+from decouple import config, Csv
 import dj_database_url
 
 
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())]
 
 
 # Application definition
